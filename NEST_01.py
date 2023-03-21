0717d3f3c22fbaca95ce2421eb62e9b2a6790b8a
Maria Besora <80537187+BesoraMaria@users.noreply.github.com>
2023-03-21 14:21:05 +0100

    Update NEST_01.py

--- NEST_01.py
+++ NEST_01.py
@@ -74,7 +74,7 @@ with st.form(key="form1"):
         del(all_lines[0])
             
         #if there is a final empty line must be deleted
-        all_lines2 = all_lines.replace('\n\n','\n').replace('\n\n','\n')
+        all_lines2 = filter(lambda x: not re.match(r'^\s*$', x), all_lines)
         
         #splitting the information of the coordinates in atoms, x y and z
         for line in all_lines2:
