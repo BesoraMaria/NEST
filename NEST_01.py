2b5cacfef7ebc55dae5e813b864f5e81df5f6b00
Maria Besora <80537187+BesoraMaria@users.noreply.github.com>
2023-03-21 14:45:34 +0100

    Update NEST_01.py

--- NEST_01.py
+++ NEST_01.py
@@ -75,7 +75,6 @@ with st.form(key="form1"):
         del(all_lines[0])
         
         #if there is a final empty line must be deleted
-        all_lines2 = all_lines.split('\n')
         all_lines2 = [line for line in all_lines if line.strip()]
        
         st.write(all_lines2)
