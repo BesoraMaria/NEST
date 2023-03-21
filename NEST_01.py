bd5a901c7174ead09056ab98314b9b1efcdf865c
Maria Besora <80537187+BesoraMaria@users.noreply.github.com>
2023-03-21 14:03:37 +0100

    Update NEST_01.py

--- NEST_01.py
+++ NEST_01.py
@@ -76,8 +76,7 @@ with st.form(key="form1"):
         #if there is a final empty line must be deleted
         #splitting the information of the coordinates in atoms, x y and z
         for line in all_lines:
-            split=line.split()
-            split=[line for line in split if line.strip('\n')]
+            split=[line for line in all_line.split() if line.strip() != '']
             atoms.append(split[0].decode())
             coor_x.append(float(split[1]))
             coor_y.append(float(split[2]))
